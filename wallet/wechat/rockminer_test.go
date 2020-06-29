f61b49903a5d3a43e6c14b851ec613c64c0cebfa
石姚嘉 <shiyaojia@rockminer.com>
2020-06-29 18:20:26 +0800

    法币支付

--- wallet/wechat/rockminer_test.go
+++ wallet/wechat/rockminer_test.go
@@ -24,12 +24,12 @@ func TestRMPay(t *testing.T) {
 	RSA := rsa.NewRSACrypt()
 	params := &ThirdPayToRM{
 		OrderSn:     "test2",
-		UserId:      "$2y$13$nchMil0i.HsMEI92UVR3ruqUJCC7F3N6FKq4RvKec6ivbk6KodMEa",
+		UserId:      "$2y$13$W6xCSp72rHldAqIXLbuyFuLkVny/q/NRLpWOQGQgtUm.SIRxuAiOO",
 		PaymentType: 11,
-		PayAmount:   0.01,
+		PayAmount:   1,
 		OrderType:   2,
 		Lang:        "en",
-		Client:      2,
+		Client:      1,
 	}
 
 	result, err := RMPay(params, RSA)
@@ -40,7 +40,7 @@ func TestRMPay(t *testing.T) {
 func Test_Decrypt(t *testing.T) {
 	Init()
 	RSA := rsa.NewRSACrypt()
-	data := "{\"data\":\"3MepupOK0mfy3SxzAeqApHgkTY7Yddymv45NEBIL7CBTBy36DzwntiHMT+8VPBxfBDjEPP1H2yadUovjPQFNjFVghi1sXF95PZvxUOW3COJec3bn3BZmicURlSH6GRxSuMfUs2ouxFt6J31+sREzK9NDe2ELaE8Mr4V1g6jnDFCbmLLKOJJnPpAB04SMD9NFAYD7ZictIwCvH8jTzrJHtq+lSR1PokRtHj9yrZWnRKxlvk\\/ST7lGDg3ZuqDCSlTkyRW+MWBBmXKmD487RXnOHvJP7LKdJCJk8Z3YFdS8+qanbmsHgbWGCjhRjN5gF2vrLe1lOAbxk43kqkY4nVXRwBTiYmIYqJvhvdgf9uhhcXdaydwRLokjetRE1wpaiTT1PPd3KWxT9T5UoJ1rXiq8KNAg+Z8DLTnjeWtht3vfGQUeZqIeGNrZRnF675NwO1GFsl8Y5pJ7xuV\\/CNYDROKTlvc3WoOcUChy4vfXgfd0NFtk4oIYl+kop0LfcIGqL7DUtX5JktwjNCpbu2lXMJsk5vJo4W\\/gAS7wkDQilDm88F\\/f4KoS5B1OX+du3+GPRDwok6pVlN4qeZzta\\/Za8W5JOWLoR9L8+WCZwK\\/BtU5cKJMHUA7VKKAa9XvBe0f8eLjkU6Nonp\\/IsK58vPq2Q3aYPeZaVvSHapEANGGfMvvGyQ0=\",\"sign\":\"c9pDHPsOwzaxc\\/ilOsWjA6LbWu8rggoO5R9NIrSqBlG3PSD\\/U2mdRMbmL55jjxIl8DcA\\/PccrlO0t0gHF+AV9tNYQsCgY8wTa5HPxTQbOZW5q4iROjHG5S89xHbGhGSa728i5nJmZ9CSbqYpg3UDpyyUgtDQ1+qtjNzoo4QcZb8vAN0IlTSPRUHCkKnXzbJoy2UNO51qYgNw\\/Y8xI66FHZNIeatxpMeZonHGSVLiWKKbwegLwWkO+ZhHTnu7q3a6znr\\/\\/uzGt\\/AWq+5SZvz7INmUyNNf3mR\\/oKW9Kvj\\/qji3FkKfXZ1wcVrZ+G64EhkdYiFXbl1azs0+9NwTSP49VyonQdfwTMX9+YoHzoKZ3U8tb94+WM8eIyWIrw1qQb\\/oAanHBfzwwHfyexFWhSvgnose94re2YHpRFOzgr4SfMo2QdaDOs6YFsJtNpuoGlMU4\\/FMNFq8smeDxfG2ittV\\/5CwsNnfA5dtpFnEd8k3dG3P9bHLqzm741c7djVl7Cya6lKxPvWz2MwNcJbC+5Ptsn+rZozChb9cF+sZEX9MD1nUJqT8jAmllRfZR9Ao5sMRR7\\/P3J9xmYcIXSPT7igVZw8BlnsdHonmZZohOGriC0diAkM+xgAKmCxw7GpQlzqT90j1fo9B3K0HqsIixXGrUMO04G+k3mrrpF4kR\\/j2gHk=\"}"
+	data := "{\"data\":\"RcWZiHi5tXi96V3a6fajykWZZ56YscOJLEYpcQ08+XPpRQEIMNA8g9xhsH1bGlBgmvvPlyHWJSE+kM0ZFPHpvUEd6aoOVu4vc4vA72TCfYgM+jeNGwFEutM+jjhOuivIly1d7Ko74xQS3LM\\/gY2yPkr4pWX8jrFTznb+oxlRpa3FgItXolbw0lxp7NnEGfp7jzWIM8Z0GuOK8LcJkMgvCVMYHXqGYlhjQJyfuvUDpnkPz3hIrlsoMYKza0j5IEXBB1e06588yQiD1N8dimEIPDl5p8MaOikLgLvJO5gSNtZwcmrQq1i3VlzjLFdKJjEGR7\\/JVk9U5\\/GuHMT1KYeNr5kpDgcGmvF2DDsWtVGrPHccCI9r+Gx8lBpNSSGnNPtkFdB798kzYkmgGno9FUJXNnrNEs7oUTaM2TPeHkzbhC2OvmrpX1EaIGhKGb0Zn7xcr+t0D+UVAYMRR4\\/FN6yB+4A+ocBWpxdurn+9BzI8aCmarzrIUo8rt8dB+h2LXyfaDuzf4Cc3WyplQ8ydou0ftOCk5UilnNoxnR\\/N1keuCwHH6kTSds\\/AtyeXq4R8EuqdNV5EXDO2zdtJcUNA9dJoiX3JWBemtP8MgklPkVGVK5KFgpIkeye+Tw5WsGbh40oVJ3xrwTp9W0J7DxWHJiQk97rNqQZ04FJ5HTz0VWa9ijI=\",\"sign\":\"c9pDHPsOwzaxc\\/ilOsWjA6LbWu8rggoO5R9NIrSqBlG3PSD\\/U2mdRMbmL55jjxIl8DcA\\/PccrlO0t0gHF+AV9tNYQsCgY8wTa5HPxTQbOZW5q4iROjHG5S89xHbGhGSa728i5nJmZ9CSbqYpg3UDpyyUgtDQ1+qtjNzoo4QcZb8vAN0IlTSPRUHCkKnXzbJoy2UNO51qYgNw\\/Y8xI66FHZNIeatxpMeZonHGSVLiWKKbwegLwWkO+ZhHTnu7q3a6znr\\/\\/uzGt\\/AWq+5SZvz7INmUyNNf3mR\\/oKW9Kvj\\/qji3FkKfXZ1wcVrZ+G64EhkdYiFXbl1azs0+9NwTSP49VyonQdfwTMX9+YoHzoKZ3U8tb94+WM8eIyWIrw1qQb\\/oAanHBfzwwHfyexFWhSvgnose94re2YHpRFOzgr4SfMo2QdaDOs6YFsJtNpuoGlMU4\\/FMNFq8smeDxfG2ittV\\/5CwsNnfA5dtpFnEd8k3dG3P9bHLqzm741c7djVl7Cya6lKxPvWz2MwNcJbC+5Ptsn+rZozChb9cF+sZEX9MD1nUJqT8jAmllRfZR9Ao5sMRR7\\/P3J9xmYcIXSPT7igVZw8BlnsdHonmZZohOGriC0diAkM+xgAKmCxw7GpQlzqT90j1fo9B3K0HqsIixXGrUMO04G+k3mrrpF4kR\\/j2gHk=\"}"
 
 	result, err := checkBase(data, RSA)
 	fmt.Printf("err is %+v \n", err)
