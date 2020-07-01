8cb8e884a0c510036b78b98ab86ab82c6befeb0a
石姚嘉 <shiyaojia@rockminer.com>
2020-07-01 15:16:51 +0800

    微信支付 回调

--- wallet/wechat/rm_callback.go
+++ wallet/wechat/rm_callback.go
@@ -2,7 +2,6 @@ package wechat
 
 type RmCallback struct {
 	OrderNo   string  `json:"order_no"`
-	Status    string  `json:"status"`
 	PayAmount float64 `json:"pay_amount"`
 	PayTime   string  `json:"pay_time"`
 	PayType   int64   `json:"pay_type"`
--- wallet/wechat/rockminer_test.go
+++ wallet/wechat/rockminer_test.go
@@ -2,27 +2,13 @@ package wechat
 
 import (
 	"fmt"
-	"github.com/RMIO2020/Go-Common/helper"
-	"github.com/RMIO2020/Go-Wallet-Service/common/helper/rsa"
-	"github.com/RMIO2020/Go-Wallet-Service/config"
 	"testing"
 )
-
-func Init() {
-	vHandle := config.New()
-	path, _ := helper.GetProjectRoot()
-	path = path + "/../../../"
-	err := vHandle.InitConfig(path)
-	if err != nil {
-		panic(err)
-	}
-	rsa.InitTRSACrypt(vHandle.Config.RMRsa)
-}
 
 func TestRMPay(t *testing.T) {
 	params := &ThirdPayToRM{
 		Platform:    "pin-min",
-		OrderSn:     "test3",
+		OrderSn:     "test4",
 		Token:       "TZcJ0hB9LnMOS3r9hrPqP9o9Upmhehhv",
 		PaymentType: 11,
 		PayAmount:   0.01,
