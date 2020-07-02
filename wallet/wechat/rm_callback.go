161a991dd251ea8b4caa7d6613ecafb798d3da60
石姚嘉 <shiyaojia@rockminer.com>
2020-07-02 12:15:59 +0800

    微信支付 回调 类型

--- wallet/wechat/rm_callback.go
+++ wallet/wechat/rm_callback.go
@@ -1,8 +1,8 @@
 package wechat
 
 type RmCallback struct {
-	OrderNo   string  `json:"order_no"`
-	PayAmount float64 `json:"pay_amount"`
-	PayTime   string  `json:"pay_time"`
-	PayType   int64   `json:"pay_type"`
+	OrderNo     string `json:"order_no"`
+	PayAmount   string `json:"pay_amount"`
+	ProcessType string `json:"process_type"`
+	PayType     int64  `json:"payment_type"`
 }
