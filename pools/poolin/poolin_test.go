2c44870d05f30f1ee6d174e241db919ddcba7899
wuyongzhan@rockminer.com <2268769732@qq.com>
2021-03-15 19:15:15 +0800

    [bug] -- 修正双挖

--- pools/poolin/poolin_test.go
+++ pools/poolin/poolin_test.go
@@ -11,7 +11,7 @@ func initConf(Puid, Token string) {
 
 func TestPoolIn_ListOfIncome(t *testing.T) {
 	//initConf("9153598", "wowrAsFD2hBxdOWwyDqpNEq0MWa8jKPl2skfAv0Svtm3x4nL7MLG2Aqns6UeoiVU")
-	initConf("9140069", "wowPrFLnaYGcbniQnXiOwTaTzqRwDdkBtF0mLzdiAZliDtlCNylBtp8Hgfu5YHgK")
+	initConf("9208485", "wowaaH1uSpH4HHmjdz8k6jmHLB2SZOjHoBxcThYFCddaVmM1imuWFppmM1PJ39P3")
 	result, err := PIN.ListOfIncome(&IncomeListParams{
 		CoinType: "zec",
 		//Page:      0,
